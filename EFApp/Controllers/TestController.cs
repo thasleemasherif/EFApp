167911b99114f0c25c8f0ee09e0ed8838164226b
thasleemasherif <thasleemasherif2256@gmail.com>
2021-12-12 12:48:55 +0530

    Add project files.

---
+++ EFApp/Controllers/TestController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFApp.Controllers
+{
+    public class TestController : Controller
+    {
+        UstDbContexts ust = new UstDbContexts();
+
+        // GET: Test
+        public ActionResult Biodata()
+        {
+            // the below given query is meant for retriving data from biodata tables
+            var students = (from std in ust.Biodatas select std).ToList();
+            return View(students);
+        }
+
+        public ActionResult AddNewStd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddNewStd(Biodata b)
+        {
+            if (ModelState.IsValid)
+            {               
+                ust.Biodatas.Add(b);
+                ust.SaveChanges(); // will execute adding data action on database
+                                   // (It will generate Insert command)
+                ViewBag.info = "Row Added....";
+            }
+            return View();
+        }
+
+        public ActionResult ShowOneStd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ShowOneStd(Biodata b)
+        {
+            int rno = b.rollno;
+            var students = (from std in ust.Biodatas where std.rollno==rno select std).FirstOrDefault<Biodata>();
+            if (students == null)
+            {
+                ViewBag.info = "No";
+                return View();
+            }
+            ViewBag.info = "Y";
+            return View(students);
+        }
+    }
+}
---
+++ EFApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFApp.MvcApplication" Language="C#" %>
